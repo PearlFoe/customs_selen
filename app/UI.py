c714c49edf3b787f6e3b60e8da5f765f8387a019
PearlFoe <itknml@gmail.com>
2021-07-20 16:31:29 +0300

    Remade GIU using tkinter

--- app/UI.py
+++ app/UI.py
@@ -1,114 +1,159 @@
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.slider import Slider
-from kivy.uix.button import Button
+from tkinter import *
+from tkinter.ttk import Frame
 
 import os
-os.environ['KIVY_GL_BACKEND']='angle_sdl2'
 
-class Vier(App):
+class Vier():
 	"""docstring for Vier"""
-	def build(self):
-		layout = BoxLayout(padding=10, orientation='vertical')
+	def __init__(self):
+		self.window = Tk()
+		self.window.geometry('550x400')
 
-		header = Header('Current orders').create()
-		order_list = OrdersList().create()
+	def run(self):
+		header = Frame(self.window)
+		Header(
+			header, 
+			'Current orders'
+		).create()
+		header.pack(side=TOP, anchor=NW)
+		
+		l_input_frame = LabelFrame(self.window)
+		input_frame = Frame(l_input_frame)
+		InputForm(input_frame).create()
 
-		layout.add_widget(header)
-		layout.add_widget(order_list)
-		layout.add_widget(Label())
+		input_frame.pack(side=BOTTOM)
+		l_input_frame.pack(side=BOTTOM)
 
-		#layout.add_widget(start_btn)
+		l_order_list_frame = LabelFrame(self.window)
+		order_list_frame = Frame(l_order_list_frame)
+		OrderList(order_list_frame).create()
 
-		return layout
+		scrollbar = Scrollbar(order_list_frame, orient=VERTICAL)
+
+		canvas = Canvas(order_list_frame, width=300, height=100,
+                                yscrollcommand=scrollbar.set)
+		scrollbar.config(canvas=canvas.yview)
+
+		order_list_frame.pack(side=LEFT)
+		scrollbar.pack(side=RIGHT, fill=Y)
+		l_order_list_frame.pack(side=TOP)
+
+		'''
+		order_frame = Frame(self.window)
+		Order(
+			order_frame, 
+			'Login',
+			'01-01-2001',
+			'00-01',
+			'Started'
+		).create()
+		
+		order_frame.pack(side=TOP)
+		'''
+		self.window.mainloop()
 
 class Header():
 	"""docstring for Header"""
-	def __init__(self, lable_text):
-		self.text = lable_text
+	def __init__(self, window, text):
+		self.window = window
+		self.text = text
 
 	def create(self):
-		layout = AnchorLayout(anchor_x='left', anchor_y='top')
-
-		label = Label(
-					text=self.text,
-					size_hint=(.17, .3),
-				)
- 		
-		layout.add_widget(label)
-
-		return layout
+		lbl = Label(self.window, text=self.text, font=("Arial Bold", 18))
+		lbl.pack(side=LEFT) 
 		
-class OrdersList():
+class OrderList():
+	"""docstring for OrderList"""
+	def __init__(self, window):
+		self.window = window
+		self.orders = []
+		
 	def create(self):
-		layout = BoxLayout(padding=25, spacing=10)
+		'''
+		for i in orders:
+			order_frame = Frame(self.window)
+			Order(order_frame).create()
+			order_frame.pack(side=TOP)
+		'''
+		for _ in range(10):
+			order_frame = Frame(self.window)
+			Order(
+				order_frame, 
+				'Login',
+				'01-01-2001',
+				'00-01',
+				'Started'
+			).create()
+			order_frame.pack(side=TOP)
 
-		for i in range(5):
-			order = Order(f'Login {i}', '23-12-2021', '00-01', 'Status').create()
-			layout.add_widget(order)
-		'''
-		slider = Slider()
-		layout.add_widget(slider)
-		'''
-		return layout
+	def add_order(self, order):
+		pass
+
+	def remove_order(self, order):
+		pass
 
 class Order():
-	"""docstring for Order"""
-	def __init__(self, login, date, time, status):
+	def __init__(self, window, login, date, time, status):
+		self.window = window
 		self.login = login
 		self.date = date
 		self.time = time
 		self.status = status
-		self.height = 0.2
-		self.width = 0.8
-
-	def create_login_lable(self):
-		lable = Label(
-					text=self.login,
-					size_hint=(self.width, self.height),
-				)
-
-		return lable
-
-	def create_datetime_lable(self):
-		text = f'Date: {self.date}\nTime: {self.time}'
-		lable = Label(
-					text=text,
-					size_hint=(self.width, self.height),
-				)
-
-		return lable
-
-	def create_status_lable(self):
-		lable = Label(
-					text=self.status,
-					size_hint=(self.width, self.height),
-				)
-
-		return lable
-
-	def create_order_delete_btn(self):
-		button = Button(
-					text='Delete',
-					size_hint=(.2, self.height),
-					background_color='red'
-				)
-
-		return button
+		self.location = {
+				'login': 0,
+				'datetime': 1,
+				'status': 2,
+		}
 
 	def create(self):
-		layout = BoxLayout(padding=5, spacing=0)
+		self.add_login()
+		self.add_datetime()
+		self.add_status()
+
+	def add_login(self):
+		label = Label(self.window, text=self.login, font=("Arial Bold", 12))
+		label.grid(column=self.location['login'], row=0, rowspan=1)
+
+	def add_datetime(self):
+		text = f'Date: {self.date}\nTime: {self.time}'
+		label = Label(self.window, text=text, font=("Arial Bold", 12))
+		label.grid(column=self.location['datetime'], row=0, rowspan=2, sticky=W)
+
+	def add_status(self):
+		label = Label(self.window, text=self.status, font=("Arial Bold", 12))
+		label.grid(column=self.location['status'], row=0, rowspan=1)
+
+class InputForm():
+	"""docstring for InputForm"""
+	def __init__(self, window):
+		self.window = window
+		self._date_input = None
+		self._time_input = None
+		self._confirm_btn = None
+
+	def create(self):
+		self.add_date_input()
+		self.add_time_input()
+		self.add_confirm_btn()
 		
-		login_ = self.create_login_lable()
-		datetime_ = self.create_datetime_lable()
-		status_ = self.create_status_lable()
-		delete_btn_ = self.create_order_delete_btn()
+	def add_date_input(self):
+		date = StringVar()
+		date.set(None)
+		_date_input = Entry(self.window, text=date, justify=LEFT)
+		label = Label(self.window, text='Date', font=("Arial Bold", 12))
 
-		layout.add_widget(login_)
-		layout.add_widget(datetime_)
-		layout.add_widget(status_)
-		#layout.add_widget(delete_btn_)
+		label.grid(row=0, column=0, sticky=W)
+		_date_input.grid(row=0, column=1, sticky=W)
 
-		return layout
+	def add_time_input(self):
+		time = StringVar()
+		time.set(None)
+		_time_input = Entry(self.window, text=time, justify=LEFT)
+		label = Label(self.window, text='Time', font=("Arial Bold", 12))
+
+		label.grid(row=1, column=0, sticky=W)
+		_time_input.grid(row=1, column=1, sticky=W) 
+
+	def add_confirm_btn(self):
+		_confirm_btn = Button(self.window, text='Add')
+		_confirm_btn.grid(row=0, column=2, sticky=E)
